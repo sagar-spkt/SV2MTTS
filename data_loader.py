e6591682087543fcb40f3f09faaacd170f7e769f
Sagar Sapkota <spkt.sagar@gmailcom>
2019-07-02 00:55:06 +0545

    Speaker Embedding preprocess

--- data_loader.py
+++ data_loader.py
@@ -1,5 +1,9 @@
+import os
 import numpy as np
+import pandas as pd
 from tensorflow.python.keras.utils import Sequence
+
+from utterance_utils import mel_for_speaker_embeddings
 
 
 class RandomTrainGenerator(Sequence):
@@ -14,5 +18,67 @@ class RandomTrainGenerator(Sequence):
                 np.random.rand(self.batch_size, 3, 160, 40),
                 np.random.rand(self.batch_size, 33, 400)], \
                [np.random.rand(self.batch_size, 33, 400),
-                np.random.rand(self.batch_size, 33*5, 1025)]
+                np.random.rand(self.batch_size, 33 * 5, 1025)]
 
+
+class SpeakerEmbeddingPredictionGenerator(Sequence):
+    def __init__(self, numpied_dir,
+                 batch_size,
+                 sliding_window_size,
+                 sample_rate,
+                 n_fft,
+                 hop_length,
+                 win_length,
+                 n_mels,
+                 ref_db,
+                 max_db
+                 ):
+        self.batch_size = batch_size
+        self.sliding_window_size = sliding_window_size
+        self.sample_rate = sample_rate
+        self.n_fft = n_fft
+        self.hop_length = hop_length
+        self.win_length = win_length
+        self.n_mels = n_mels
+        self.ref_db = ref_db
+        self.max_db = max_db
+
+        df = pd.read_csv(os.path.join(numpied_dir, 'trans.tsv'), header=None, sep='\t')
+        df['len'] = df[2].str.len()
+        df = df.sort_values('len').reset_index(drop=True)
+        ids = np.array(list(df[0].str.split('_')))
+        self.all_utterances = os.path.abspath(numpied_dir) + '/' + pd.Series(ids[:, 0]) + '/' + \
+                              pd.Series(ids[:, 1]) + '/' + df[0] + '.npy'
+
+    def __len__(self):
+        return len(self.all_utterances) // self.batch_size + 1
+
+    def get_all_utterances(self):
+        return list(self.all_utterances)
+
+    def __getitem__(self, index):
+        current_batch = self.all_utterances[index * self.batch_size: (index + 1) * self.batch_size]
+        mel_specs = [
+            mel_for_speaker_embeddings(utt, sample_rate=self.sample_rate, n_fft=self.n_fft, hop_length=self.hop_length,
+                                       win_length=self.win_length, n_mels=self.n_mels, ref_db=self.ref_db,
+                                       max_db=self.max_db) for utt in current_batch]
+        mel_slided = [np.stack(
+            [utt[i: i + self.sliding_window_size] for i in range(0, utt.shape[0], int(self.sliding_window_size // 2)) if
+             (i + self.sliding_window_size) <= utt.shape[0]]) for utt in mel_specs]
+        # padding
+        max_len = np.max([utt.shape[0] for utt in mel_slided])
+        padded_mel_slides = np.stack(
+            [np.pad(utt, ([0, max_len - utt.shape[0]], [0, 0], [0, 0]), mode='constant') for utt in mel_slided], axis=0)
+
+        return padded_mel_slides
+
+
+class SynthesizerTrainGenerator(Sequence):
+    def __init__(self):
+        pass
+
+    def __len__(self):
+        pass
+
+    def __getitem__(self, index):
+        pass
--- preprocess.py
+++ preprocess.py
@@ -5,6 +5,9 @@ import numpy as np
 import pandas as pd
 from tqdm import tqdm
 
+from data_loader import SpeakerEmbeddingPredictionGenerator
+from models import get_speaker_embedding_model
+
 
 def wav_to_numpy(dataset_rootdir, preprocess_rootdir, sample_rate, min_len):
     dataset_rootdir = os.path.abspath(dataset_rootdir)
@@ -26,7 +29,7 @@ def wav_to_numpy(dataset_rootdir, preprocess_rootdir, sample_rate, min_len):
     for utterance in tqdm(all_utterances):
         y, _ = librosa.load(utterance, sr=sample_rate)
         y, _ = librosa.effects.trim(y)
-        if len(y) < int(min_len*sample_rate):
+        if len(y) < int(min_len * sample_rate):
             continue
         total_time += len(y) / sample_rate
         max_len = np.max((max_len, len(y)))
@@ -41,3 +44,42 @@ def wav_to_numpy(dataset_rootdir, preprocess_rootdir, sample_rate, min_len):
         f.write(str(total_time) + '\t' + str(max_len))
     print('Done')
 
+
+def wav_to_speaker_embeddings(numpied_dir,
+                              model_path,
+                              batch_size,
+                              sample_rate,
+                              n_fft,
+                              hop_length,
+                              win_length,
+                              n_mels,
+                              ref_db,
+                              max_db,
+                              sliding_window_size,
+                              spk_embed_lstm_units,
+                              spk_embed_size,
+                              spk_embed_num_layers,
+                              verbose=1):
+    speaker_embedding_model = get_speaker_embedding_model(sliding_window_size=sliding_window_size,
+                                                          embed_mels=n_mels,
+                                                          spk_embed_lstm_units=spk_embed_lstm_units,
+                                                          spk_embed_size=spk_embed_size,
+                                                          spk_embed_num_layers=spk_embed_num_layers)
+    speaker_generator = SpeakerEmbeddingPredictionGenerator(numpied_dir,
+                                                            batch_size=batch_size,
+                                                            sliding_window_size=sliding_window_size,
+                                                            sample_rate=sample_rate,
+                                                            n_fft=n_fft,
+                                                            hop_length=hop_length,
+                                                            win_length=win_length,
+                                                            n_mels=n_mels,
+                                                            ref_db=ref_db,
+                                                            max_db=max_db)
+    speaker_embedding_model.load_weights(model_path, by_name=True)
+    speaker_embeddings = speaker_embedding_model.predict_generator(speaker_generator, verbose=verbose)
+
+    iterator = tqdm(zip(speaker_generator.get_all_utterances(), speaker_embeddings)) if verbose else zip(
+        speaker_generator.get_all_utterances(), speaker_embeddings)
+
+    for utterance, embedding in iterator:
+        np.save(utterance.replace('.npy', '_embed.npy'), embedding)
